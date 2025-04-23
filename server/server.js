5639f503a1cef2181ba4e1d2393ad6d1b4b8bd1d
mario ruby <mariogabrielrn@gmail.com>
2025-04-23 18:54:26 +0200

    keep alive

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import app from './app.js';
 import dotenv from 'dotenv';
+import { keepAlive } from './routes/keepAlive.js';
 
 dotenv.config();
 
@@ -7,4 +8,5 @@ const PORT = process.env.PORT || 5005;
 
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`);
+    keepAlive();
 });
